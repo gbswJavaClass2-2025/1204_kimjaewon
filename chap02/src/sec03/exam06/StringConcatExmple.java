6b06069842106a977cb0a1d5353d0630abc479f8
1one-999 <2025sw20@sc.gyo6.net>
2025-09-09 09:34:47 +0900

    챕터 2 완료

---
+++ chap02/src/sec03/exam06/StringConcatExmple.java
@@ -0,0 +1,22 @@
+package sec03.exam06;
+
+public class StringConcatExmple {
+
+	public static void main(String[] args) {
+		int value = 10 + 2 + 8;
+		System.out.println("value: " + value);
+
+		String str1 = 10 + 2 + "8";
+		System.out.println("value: " + str1);
+
+		String str2 = 10 + "2" + 8;
+		System.out.println("value: " + str2);
+
+		String str3 = "10" + 2 + 8;
+		System.out.println("value: " + str3);
+
+		String str4 = "10" + (2 + 8);
+		System.out.println("value: " + str4);
+	}
+
+}
