1099e7bdc34b1fa9033ea09129f79fc95007f02e
1one-999 <2025sw20@sc.gyo6.net>
2025-09-10 21:24:25 +0900

    9/10일 과제 완료

---
+++ chap03/src/sec02/exam06/StringConcatExample.java
@@ -0,0 +1,9 @@
+package sec02.exam06;
+
+public class StringConcatExample {
+	public static void main(String[] args) {
+		String str1 = "JDK" +6.0;
+		String str2 = str1 + " 특";
+		System.out.println(str2);
+	}
+}
